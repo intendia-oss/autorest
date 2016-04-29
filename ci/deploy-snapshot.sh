d37ce6095c043ff106af2f7bb0d947d27f269a19
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-04-29 21:43:01 +0200

    Configure travis to deploy snapshot to sonatype

---
+++ ci/deploy-snapshot.sh
@@ -0,0 +1,7 @@
+if [ "$TRAVIS_REPO_SLUG" == "intendia-oss/autorest-gwt" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  mvn -s ci/settings.xml clean source:jar deploy -Dmaven.test.skip=true -Dinvoker.skip=true
+fi
