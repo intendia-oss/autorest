b73cd841d83e473ba960733b753e5b4cfe698db8
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-09-13 23:44:22 +0200

    Rename RestServiceModel.parent to path

--- core/src/main/java/com/intendia/gwt/autorest/client/RestServiceModel.java
+++ core/src/main/java/com/intendia/gwt/autorest/client/RestServiceModel.java
@@ -1,13 +1,13 @@
 package com.intendia.gwt.autorest.client;
 
 public class RestServiceModel {
-    protected final ResourceVisitor.Supplier parent;
+    protected final ResourceVisitor.Supplier path;
 
-    public RestServiceModel(ResourceVisitor.Supplier parent) {
-        this.parent = parent;
+    public RestServiceModel(ResourceVisitor.Supplier path) {
+        this.path = path;
     }
 
     protected ResourceVisitor method(String method) {
-        return parent.get().method(method);
+        return path.get().method(method);
     }
 }
