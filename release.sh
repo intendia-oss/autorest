1e6d2a291944257882c5f7226d62c7bc2e8e3478
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2017-04-26 09:48:24 +0200

    Apply HEAD-SNAPSHOT versioning strategy

--- release.sh
+++ release.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mvn release:prepare release:perform && git push
+if [ -z "$1" ];
+then echo "usage: release.sh <releaseVersion>"
+else mvn release:prepare release:perform -DreleaseVersion=$1 -Dtag=v$1
+fi
