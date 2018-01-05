8e4c85a27f4228c403cd6d029b3f9ea9ced83127
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2018-01-05 09:36:35 +0100

    Remove unused resources

--- ci/deploy-snapshot.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_REPO_SLUG" == "intendia-oss/autorest" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
-   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
-   [ "$TRAVIS_BRANCH" == "master" ]; then
-
-  mvn -s ci/settings.xml clean source:jar deploy -Dmaven.test.skip=true -Dinvoker.skip=true
-fi
--- release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ];
-then echo "usage: release.sh <releaseVersion>"
-else mvn release:prepare release:perform -DreleaseVersion=$1 -Dtag=v$1
-fi
